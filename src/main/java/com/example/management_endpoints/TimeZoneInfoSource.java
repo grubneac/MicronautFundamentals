c938f5388a3ed58385ed26b0682680e90a35aa0d
AlexG <grubneac@yandex.ru>
2026-01-05 12:51:27 +0200

    management endpoints

---
+++ src/main/java/com/example/management_endpoints/TimeZoneInfoSource.java
@@ -0,0 +1,21 @@
+package com.example.management_endpoints;
+
+import io.micronaut.context.env.MapPropertySource;
+import io.micronaut.context.env.PropertySource;
+import io.micronaut.core.async.publisher.Publishers;
+import io.micronaut.management.endpoint.info.InfoSource;
+import jakarta.inject.Singleton;
+import org.reactivestreams.Publisher;
+
+import java.time.ZoneId;
+import java.util.Map;
+
+@Singleton
+public class TimeZoneInfoSource implements InfoSource {
+    @Override
+    public Publisher<PropertySource> getSource() {
+        return Publishers.just(
+                new MapPropertySource("timeZone", Map.of("timeZone", ZoneId.systemDefault().toString()))
+        );
+    }
+}
